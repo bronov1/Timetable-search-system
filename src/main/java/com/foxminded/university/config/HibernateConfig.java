1be73d5ffe3fba8737e35076b706a3cdc3a900e6
Illia Bronov <illia.bronov@nure.ua>
2021-01-11 23:57:31 +0200

    Application works on boot, but with configuration

--- src/main/java/com/foxminded/university/config/HibernateConfig.java
+++ src/main/java/com/foxminded/university/config/HibernateConfig.java
@@ -9,7 +9,6 @@ import org.hibernate.cfg.Environment;
 import org.hibernate.service.ServiceRegistry;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
---
+++ src/main/java/com/foxminded/university/config/SpringMvcDispatcherSerlvetInitialaizer.java
@@ -0,0 +1,21 @@
+package com.foxminded.university.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringMvcDispatcherSerlvetInitialaizer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringMVCConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
